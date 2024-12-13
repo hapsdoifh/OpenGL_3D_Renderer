34ead347dc6973285ec91c9d21091c95a9f5c7b0
= <=>
2024-12-13 01:10:02 -0500

    Added glfw libraries and includes, configured cmakelist, successfully created a blank window

--- OpenGL_3D_Graphics_1.0/source/GLWindow.h
+++ OpenGL_3D_Graphics_1.0/source/GLWindow.h
@@ -1,8 +1,8 @@
 #pragma once
-#include <QtOpenGLWidgets/qopenglwidget.h>
 #include <glm/glm.hpp>
+#include <glad.h>
 
-class GLWindow : public QOpenGLWidget{
+class GLWindow{
 	
 	
 public:
--- OpenGL_3D_Graphics_1.0/source/OpenGL_3D_Graphics_1.0.cpp
+++ OpenGL_3D_Graphics_1.0/source/OpenGL_3D_Graphics_1.0.cpp
@@ -1,22 +1,53 @@
 // source.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
-//#include <iostream>
-#include <QtWidgets/qapplication.h>
 #include <GLWindow.h>
+#include <iostream>
+#include <glfw3.h>
+
+static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods) {
+    if(key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
+        glfwSetWindowShouldClose(window, GLFW_TRUE);
+    }
+}
 
 int main(int argc, char* argv[])
 {
-   /* QApplication app(argc, argv);
-    GLWindow testWindow;
-    testWindow.resize(1000, 600);
-    testWindow.show();
-    return app.exec();*/
+    if(!glfwInit()) {
+        std::cout << "glfw initialization failed, exitting";
+        exit(0);
+    }
+    GLFWwindow* myWindow = glfwCreateWindow(640, 480, "OpenGL_Window", NULL, NULL);
+    glfwMakeContextCurrent(myWindow);
+    // std::cout << gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
+    // if(!myWindow) {
+    //     glfwTerminate();
+    //     exit(0);
+    // }
+    // GLenum err = glGetError();
+    // if (err != GL_NO_ERROR) {
+    //     std::cerr << "OpenGL error: " << err << std::endl;
+    // }
+    gladLoadGL();
+    glfwSetKeyCallback(myWindow, key_callback);
+    while(!glfwWindowShouldClose(myWindow)) {
+        int width, height;
+        glfwGetFramebufferSize(myWindow, &width, &height);
+        glViewport(0,0, width, height);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // glfwSwapBuffers(myWindow);
+        //absolutely neccessary otherwise you get unclosable transparent window that hogs resources
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(myWindow);
+    glfwTerminate();
     return 0;
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+// Debug prograam: F5 or Debug > Start Debugging menu
 
 // Tips for Getting Started: 
 //   1. Use the Solution Explorer window to add/manage files
